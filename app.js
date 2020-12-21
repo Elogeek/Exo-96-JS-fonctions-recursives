db0cede67ca0621d6135fa8db681fb974d248407
Elogeek <68891109+Elogeek@users.noreply.github.com>
2020-12-21 16:00:45 +0100

    exo 96

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-function test(a) {
+function test(a1) {
     let div = document.createElement("div");
-    div.innerHTML = "La variable a vaut " + a;
+    div.innerHTML = "La variable a vaut " + a1;
     document.body.appendChild(div);
-    ((a += 2) < 50) ? test(a) : null;
+    ((a += 2) < 50) ? test(a1) : null;
 }
 test(1);
